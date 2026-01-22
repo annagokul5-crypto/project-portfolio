b7157dcc16c74b9c0e7b673bdbd7eefa51177473
Gokulraju <gokulraju@example.com>
2026-01-22 12:24:24 +0530

    Add logs to deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+echo "RUNNING: 00-laravel-deploy.sh"
 composer install --no-dev --working-dir=/var/www/html
+echo "RUNNING: php artisan config:cache"
 php artisan config:cache
+echo "RUNNING: php artisan route:cache"
 php artisan route:cache
+echo "RUNNING: php artisan view:cache"
 php artisan view:cache
+echo "RUNNING: php artisan migrate --force"
 php artisan migrate --force
+echo "DONE: 00-laravel-deploy.sh"
