9dd4217ef5deae5ac427c95f8974cd8be9ef5638
Gokulraju <gokulraju@example.com>
2026-01-30 12:26:04 +0530

    update css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Setting;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        URL::forceScheme('https');
+
         View::composer('*', function ($view) {
             $footerYear = optional(Setting::find('footer_year'))->value ?? date('Y');
             $view->with('footerYear', $footerYear);
